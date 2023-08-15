171cf3221f531432c578a155b31cd696ccbcdb82
brianmorgan12 <142330882+brianmorgan12@users.noreply.github.com>
2023-08-15 10:19:42 -0600

    Add files via upload

---
+++ Actividad 1.cpp
@@ -0,0 +1,53 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+int main (int argc,char **argv)
+{
+	system("color 5f");
+	setlocale(LC_ALL, "spanish");
+	
+	printf("Las generarciones de las computadoras y sus caracteristicas\n"
+	
+	"1._Usaban válvulas para el procesamiento\n"
+    "2._Programadas en lenguaje de máquina\n"
+	"3._Consumían mucha energía y generaban mucho calor\n"
+	"\n"
+	"\t2da Generación (1956-1963) - Computadoras de Transistores:\n"
+	"1._Utilizaban transistores en lugar de válvulas, lo que las hacía más pequeñas y eficientes.,\n"
+	"2._Se desarrollaron lenguajes de programación de alto nivel como COBOL y FORTRAN\n"
+	"3._Se introdujo el concepto de memoria RAM.\n"
+	"\n"
+	"\t3ra Generación (1964-1971) - Circuitos Integrados:\n"
+	"1._Se introdujeron los microprocesadores, lo que permitió tener una CPU completa en un solo chip.\n"
+	"2._Aparecieron las computadoras personales y estaciones de trabajo.\n"
+	"3._Surgieron lenguajes de programación orientados a objetos como C++.\n"
+	"\n"
+	"\t4ta Generación (1971-1989) - Microprocesadores:\n"
+	"1._Circuitos Integrados: Se desarrollaron circuitos integrados (chips) que contenían múltiples transistores,\n"
+	"2._permitiendo una mayor densidad de componentes. \n"
+	"3._Minicomputadoras: Aparecieron las minicomputadoras, más pequeñas y asequibles que sus predecesoras,\n"
+	"4._pero aún con potencia de cálculo razonable. \n"
+	"5._Multiprogramación: Se introdujo la multiprogramación,\n"
+	"6._que permitía a las computadoras realizar varias tareas simultáneamente.\n"
+	"\n"
+	"\t5ta Generación (1989-presente) - Computadoras de Alto Rendimiento y AI:\n"
+	"1._Microprocesadores: Los microprocesadores fueron introducidos, lo que permitió tener una\n"
+	"2._computadora completa en un solo chip. Esto impulsó la creación de computadoras\n"
+	"3._personales y contribuyó al aumento de la potencia de cálculo a nivel individual.\n"
+	"4._Computadoras personales: La popularidad de las computadoras personales creció\n"
+	"5._enormemente durante esta generación, con máquinas como la IBM PC y la Apple\n"
+	"6._Macintosh. Las computadoras se volvieron más accesibles para un público más amplio.\n"
+	"7._Interfaces gráficas y sistemas operativos amigables: Surgieron las interfaces gráficas de\n"
+	"8._usuario (GUI) y los sistemas operativos amigables, como el Apple Macintosh System y el\n"
+	"9._sistema operativo Windows. Estos avances hicieron que las computadoras fueran más fáciles de usar para usuarios no técnicos.\n");
+
+
+
+
+	
+	
+	return 0;
+}
+
+
+	
